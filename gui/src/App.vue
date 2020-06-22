e5bda166008d6bf572f2605029c81fdc2a02ee72
grivera <grivera@multisystems.com>
2020-06-22 19:11:31 -0400

    Removed default helloworld component added top bar

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopBar></TopBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
